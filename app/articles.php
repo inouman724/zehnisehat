1de00fd02276466479c93801d6af030fd3b503bd
waqar <muhammadwakkar777@gmail.com>
2023-09-15 12:48:56 +0500

    category

--- app/articles.php
+++ app/articles.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class articles extends Model
 {
     //
+    public function category(){
+        return $this->belongsTo('App\category','category_id', 'id');
+    }
+    public function published_by(){
+        return $this->belongsTo('App\User','published_by', 'id');
+    }
 }
